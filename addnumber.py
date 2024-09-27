ba62b92e98f991e25958d55a6bf01fab3921e312
Brian-Tarimo <briantarimo45@gmail.com>
2024-09-27 20:42:20 +0300

    python cat

---
+++ addnumber.py
@@ -0,0 +1,6 @@
+def add_number(num1, num2):
+    return num1 + num2
+
+# Example usage
+result = add_number(5, 3)
+print("The sum is:", result)
---
+++ square.py
@@ -0,0 +1,6 @@
+def square(number):
+    return number ** 2
+
+# Example usage
+result = square(4)
+print("The square is:", result)
